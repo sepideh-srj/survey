5ac36a41f8c9036307c711bfd18af01d7f9f6532
sepide <sepide.sarajian@gmail.com>
2020-03-23 20:41:25 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,13 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h&+7pq58)ryit9oy@+$xr5g3_3@6pacyck+e)$*b7y11gbsgj5'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://sepidsrj.pythonanywhere.com']
 
 
 # Application definition
